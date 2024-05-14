9b9da1ddb37b0a7d8d628d537fda1981cfee865b
Your Name <you@example.com>
2024-05-14 20:41:31 +0300

    adding global state and add to cart function

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import "./App.css"
 
 import { Home } from "./Pages/home";
 import { Dashboard } from "./Pages/dashboard";
+import { createContext, useState } from "react";
+import { Product } from "./types";
 
 
 
@@ -19,14 +21,39 @@ const router = createBrowserRouter([
   },
 ]);
 
-function App() {
-  
+type GlobalContextType = {
+  state: GlobalState
+  handleAddToCart: (product: Product)=> void 
+}
 
+type GlobalState = {
+  cart: Product[]
+}
+export const GlobalContext = createContext<GlobalContextType | null>(null)
+
+function App() {
+  const [state, setState] = useState<GlobalState>({
+    cart: []
+  })
+
+  const handleAddToCart = (product: Product) => {
+    setState({
+      ...state,
+      cart:[...state.cart, product]
+    })
+  }
+
+  // const handleRemoveFromCart = (product: Product) => {
+  //   setState({  - fill from the array,then update the state with the new filter 
+  //     ...state, - then pass it to the context then take it from there 
+  //     cart:[...state.cart, product]
+  //   })
+  // }
   return (
     <div className="App">
+      <GlobalContext.Provider value = {{state, handleAddToCart}}>
      <RouterProvider router={router} /> 
-
-      
+     </GlobalContext.Provider>
     </div>
   )
 }
--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -1,13 +1,18 @@
+import { GlobalContext } from "@/App"
 import api from "@/api"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Select } from "@/components/ui/select"
 import { Product } from "@/types"
 import { useQuery } from "@tanstack/react-query"
+import { useContext } from "react"
 
 
  export function Home (){
-
+    const context = useContext(GlobalContext)
+   if(!context) throw Error("Context is missing")
+    const {state, handleAddToCart} = context 
+  
     const getProducts = async () => {
         try {
           const res = await api.get("/products")
@@ -32,31 +37,21 @@ import { useQuery } from "@tanstack/react-query"
     return(
      <>    
         <h1 className="text-2xl uppercase mb-10">Products</h1>
-       
-          <Select title="product" name="products"onChange={onChange}>
-          {data?.map((product) => {
-            return(
-            <option key={product.id} value = {product.id} >{product.id}
-              </option>
-              )
-               })} 
-          </Select>    
-
-
+        <h3>Cart ({state.cart.length})</h3>
            <section className="flex flex-col md:flex-row gap-4 max-w-6xl mx-auto flex-wrap">
-           {data?.map((product) => (
-           <Card key={product.id} className="w-[250px]">
-            <CardHeader>
-            <CardTitle>{product.name}</CardTitle>
-            <CardDescription>Some Description here</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content Here</p>
-          </CardContent>
-          <CardFooter>
-            <Button className="w-full">Add to cart</Button>
-          </CardFooter>
-        </Card>
+              {data?.map((product) => (
+              <Card key={product.id} className="w-[270px]">
+              <CardHeader>
+              <CardTitle>{product.name}</CardTitle>
+              <CardDescription>Read More..</CardDescription>
+              </CardHeader>
+              <CardContent>
+              <p>Card Content Here</p>
+              </CardContent>
+              <CardFooter>
+              <Button className="w-full" onClick={() => handleAddToCart(product)}>Add to cart</Button>
+              </CardFooter>
+              </Card>
          ))}
          </section>
          {error && <p className="text-red-500">{error.message}</p>}
